f67a855fa90def7e5fe67e111a31c4d8ba268f10
Alexander Hartmann <alex_ha@live.de>
2022-05-02 11:26:19 +0200

    make upgrade_path dynamic

--- dark_castle.py
+++ dark_castle.py
@@ -17,7 +17,7 @@ class DarkCastleBot(Bot):
         self._load_game(self._map)
         self._wait_for_map_load()
 
-        Tower(self, config.HOTKEY_HERO, config.MAPS_POS_DARK_CASTLE_INTERSECTION_TOP, None, 'hero')
+        Tower(self, 'hero', config.HOTKEY_HERO, config.MAPS_POS_DARK_CASTLE_INTERSECTION_TOP)
 
         logging.info('Starting game')
         self._start_game()
@@ -25,8 +25,7 @@ class DarkCastleBot(Bot):
         logging.info("Waiting until round 4")
         self.wait_for(config.MAPS_ROUND_DARK_CASTLE_4)
 
-        ninja = Tower(self, config.HOTKEY_TOWER_NINJA, config.MAPS_POS_DARK_CASTLE_INTERSECTION_BOTTOM,
-                      config.TOWER_NINJA_UPGRADE, 'ninja')
+        ninja = Tower(self, 'ninja', config.HOTKEY_TOWER_NINJA, config.MAPS_POS_DARK_CASTLE_INTERSECTION_BOTTOM)
         ninja.upgrade(1, 1)
         ninja.upgrade(3, 1)
         ninja.upgrade(1, 3)
@@ -34,8 +33,7 @@ class DarkCastleBot(Bot):
         logging.info("Waiting until round 20")
         self.wait_for(config.MAPS_ROUND_DARK_CASTLE_20)
 
-        super_monkey = Tower(self, config.HOTKEY_TOWER_SUPER_MONKEY, config.MAPS_POS_DARK_CASTLE_TOP_LEFT_MAIN_ROAD,
-                             config.TOWER_SUPER_UPGRADE, 'super')
+        super_monkey = Tower(self, 'super', config.HOTKEY_TOWER_SUPER_MONKEY, config.MAPS_POS_DARK_CASTLE_TOP_LEFT_MAIN_ROAD)
         super_monkey.upgrade(1, 2)
         super_monkey.upgrade(3, 2)
 
--- tower.py
+++ tower.py
@@ -7,14 +7,13 @@ import config
 
 
 class Tower:
-    def __init__(self, bot, hotkey, position, upgrade_path, name='tower'):
+    def __init__(self, bot, name, hotkey, position):
         self._name = name
         self._bot = bot
         self._bot.wait_for(position)
         self._position = self._bot.wait_location
         self._hotkey = hotkey
         self._upgrades = [0, 0, 0]
-        self._upgrade_path = upgrade_path
         logging.info('Placing {}'.format(self._name))
         pag.moveTo(self._position)
         keyboard.send(self._hotkey)
@@ -39,7 +38,7 @@ class Tower:
         for i in range(to_level - self._upgrades[track - 1]):
             tower_upgrade_text = self._tower_upgrades_to_string(track, self._upgrades[track - 1] + i + 1)
             logging.info('Waiting for {} {} to become available'.format(self._name, tower_upgrade_text))
-            self._bot.wait_for(self._upgrade_path.format(track, self._upgrades[track - 1] + i + 1))
+            self._bot.wait_for('resources/towers/{}/upgrade_{}_{}.jpg'.format(self._name, track, self._upgrades[track - 1] + i + 1))
             self._upgrade_track(track)
             logging.info('Upgraded {} to {}'.format(self._name, tower_upgrade_text))
         self._upgrades[track - 1] = to_level
